6d3c429e24bd22333635f62c8576646ab7f401b6
James F. Thomas <jthomas@rsimail.com>
2022-09-06 09:54:15 -0500

    first service created, AuthService.ts

---
+++ src/services/AuthService.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+import { User } from '../types/User'
+class AuthService {
+    public async login( userName: string, password: string ): Promise<User | undefined>{
+        if( userName === 'user' && password === '1234' ) {
+            return {
+                userName: userName,
+                email:'some@email.com'
+            }
+
+        }
+        else{
+            return undefined;
+        }
+
+    }
+} 
+
+export default AuthService;

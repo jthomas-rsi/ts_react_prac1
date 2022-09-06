3ff355eb3065164f75fa705ea3c266be3aab4852
James F. Thomas <jthomas@rsimail.com>
2022-09-06 12:27:06 -0500

    conditional rendering of loginMessage in Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,66 +1,93 @@
 import React, { ChangeEvent, SyntheticEvent } from "react";
-// import AuthService 
-import { AuthService } from '../services/AuthService'
+// import AuthService
+import { AuthService } from "../services/AuthService";
 
-// define key values of component intial state and props  
+// define key values of component intial state and props
 interface LoginProps {
-  authService: AuthService
+  authService: AuthService;
 }
 interface LoginState {
-  userName: string,
-  password: string,
-  LoginAttempted: boolean, 
-  loginSuccesfull: boolean
+  userName: string;
+  password: string;
+  LoginAttempted: boolean;
+  loginSuccesfull: boolean;
 }
 
 // create and export Login Component
-export class Login extends React.Component <LoginProps, LoginState > {
-
+export class Login extends React.Component<LoginProps, LoginState> {
   //intialize intiail state values
   state: LoginState = {
-    userName: '',
-    password: '',
+    userName: "",
+    password: "",
     LoginAttempted: false,
-    loginSuccesfull: false
-  }
+    loginSuccesfull: false,
+  };
 
   //create local functions for respodning to Login form events
-  private setUserName( event: ChangeEvent ) {
-    this.setState({ userName: (event.target as HTMLInputElement).value })
+  private setUserName(event: ChangeEvent) {
+    this.setState({ userName: (event.target as HTMLInputElement).value });
   }
 
-  private setPassword( event: ChangeEvent ) {
-    this.setState({ password: (event.target as HTMLInputElement).value })
+  private setPassword(event: ChangeEvent) {
+    this.setState({ password: (event.target as HTMLInputElement).value });
   }
 
-  private async handleSubmit( event: SyntheticEvent){
+  private async handleSubmit(event: SyntheticEvent) {
     //prevent Login component for re-rendering
     event.preventDefault();
-    // use authService worker to Login with form data 
+
+    // update loginAttempted prop
+    this.setState({ LoginAttempted: true });
+
+    // use authService worker to Login with form data
     const result = await this.props.authService.login(
       this.state.userName,
       this.state.password
-    )
-    // lof result of login attempt 
-    if( result ){
-      console.log( result )
-    }
-    else{
-      console.log('Inccorect Login')
+    );
+
+    // log result of login attempt
+    //update LoginState loginSuccesfull value
+    if (result) {
+      console.log(result);
+      this.setState({ loginSuccesfull: true });
+    } else {
+      console.log("Inccorect Login");
+      this.setState({ loginSuccesfull: false });
     }
   }
 
   //render Login Component with Form to capture data
-    render() {
-      return (
-        <div>
-          <h1> Please Login </h1>
-          <form onSubmit={ e => this.handleSubmit(e) } >
-            <input  type='text' value={ this.state.userName } onChange={ e=> this.setUserName(e) } ></input><br/>
-            <input  type='pasword' value={ this.state.password } onChange={ e=> this.setPassword(e) } ></input><br/>
-            <button type='submit' >Login</button>
-          </form>
-        </div>
-      )
+  render() {
+    //define login sucess message
+    let loginMessage: any;
+    if (this.state.LoginAttempted) {
+      if (this.state.loginSuccesfull) {
+        loginMessage = <div> You're all good</div>;
+      } else {
+        loginMessage = <div> Try again </div>;
+      }
     }
+
+    return (
+      <div>
+        <h1> Please Login </h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.userName}
+            onChange={(e) => this.setUserName(e)}
+          ></input>
+          <br />
+          <input
+            type="pasword"
+            value={this.state.password}
+            onChange={(e) => this.setPassword(e)}
+          ></input>
+          <br />
+          <button type="submit">Login</button>
+        </form>
+        {loginMessage}
+      </div>
+    );
   }
+}

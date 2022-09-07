41f4fa2a7b5bb6df94b4505abab36d1183065cc5
James F. Thomas <jthomas@rsimail.com>
2022-09-07 17:18:18 -0500

    new interface for user attributes implemented

---
+++ src/types/Types.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//intial user interface/template
+export interface User {
+    userName: string,
+    email: string,
+};
+
+
+//user attribute template/interface
+export interface UserAttribute {
+    Name: string,
+    Value: string
+}

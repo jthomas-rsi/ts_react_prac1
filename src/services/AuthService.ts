ba693ac60b5d5f2a4cae968e4894210ad3e4f8f8
James F. Thomas <jthomas@rsimail.com>
2022-09-07 17:55:45 -0500

    new async method to get user attributes

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,6 +1,8 @@
 import React from 'react';
-import { User } from '../types/Types'
+import { User, UserAttribute } from '../types/Types'
 export class AuthService {
+
+    // async method for authenticating user 
     public async login( userName: string, password: string ): Promise<User | undefined>{
         if( userName === 'user' && password === '1234' ) {
             return {
@@ -12,6 +14,32 @@ export class AuthService {
         else{
             return undefined;
         }
-
     }
+
+    // async method for retrieveing user Attributes
+    public async getUserAttributes(user: User):Promise<UserAttribute[]> {
+        const result: UserAttribute[] = [];
+        result.push({
+            Name: 'User',
+            Value: 'James F. Thomas'
+        }, 
+        {
+            Name: 'job',
+            Value: 'Engineer'
+
+        }, 
+        {
+            Name: 'age',
+            Value: '38'
+
+        }, 
+        {
+            Name: 'experience',
+            Value: '3 years'
+
+        });
+        return result;
+    }
+
+
 };

95852fba8023c7dd5975f826a16666b30612d581
James F. Thomas <jthomas@rsimail.com>
2022-09-14 09:46:30 -0500

    created res worker for res feature in Spaces.tsx

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,24 +1,25 @@
 import React from "react";
 import { Space } from "../types/Types";
 
-//data service worker that retrieves rental space information 
+//data service worker class and it's methods  
 export class DataService {
     
+    // method to retrieve rental space data objects
     public async getSpaces(): Promise <Space[]>{
         const result: Space[] = []
         //push dummy data into result array for Spaces component to render
         result.push({
-            spaceId: '001',
+            spaceId: '123',
             spaceName: 'The Awesome Building',
             address: 'Jackson, MS',
         },
         {
-            spaceId: '002',
+            spaceId: '124',
             spaceName: 'The Cool Building',
             address: 'Jackson, MS',
         },
         {
-            spaceId: '003',
+            spaceId: '125',
             spaceName: 'The Hot Building',
             address: 'Jackson, MS',
         }
@@ -26,4 +27,16 @@ export class DataService {
         )
         return result
     }
+
+    //method that allows user to reseve a space 
+    public async reserveSpace( spaceId: string): Promise < string | undefined > {
+        if( spaceId === '125' ){
+            return('5555')
+        }
+        else{
+            return undefined
+        }
+    }
+
+
 }
